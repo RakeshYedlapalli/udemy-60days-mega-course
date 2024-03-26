6827e6310b98b19356b7a5b09a7a064d59caf6c8
RakeshYedlapalli <rak.mdg94@gmail.com>
2024-03-26 22:33:49 +0800

    Implemented the weather report dashboard

--- daily-course-practicea/8-app5-weather-data-api-section-33/main.py
+++ daily-course-practicea/8-app5-weather-data-api-section-33/main.py
@@ -1,19 +1,55 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
 
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
+# filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
+# df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", dynamicData=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     temperate = 20
-    list1 = [{"rakes": "rakes", "dsaf": "fadfa"}]
-    return {"station": station, "date": date, "temperature": temperate}
+    # df = pd.read_csv("data_small/TG_STAID000001.txt", skiprows=20)
+    filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
+    print("Station name is ->", station)
+    # print(df[10:20])
+    # list1 = [{"rakes": "rakes", "dsaf": "fadfa"}]
+    return {"station": station, "date": date, "temperature": temperature}
+
+
+@app.route("/api/v1/word/<wordname>")
+def words(wordname):
+    temperate = 20
+    data = [{"definition": "cushion placed under the head for sleeping", "word": "pillow"},
+            {
+                "definition": "a device that determines the presence and properties of a substance by measuring the light it absorbs or emits.",
+                "word": "spectrophotometer"},
+            {"definition": "a horse kept for breeding", "word": "stallion"}]
+
+    found_object = find_word(data, wordname)
+
+    if found_object:
+        return found_object
+    else:
+        return "NO Object found with given name"
+
+
+def find_word(data, word):
+    for item in data:
+        if item["word"] == word:
+            return item
+    return None
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=4001)
---
+++ daily-course-practicea/9-jupyter-labs-section-34-day-30/README.md
@@ -0,0 +1,13 @@
+# to bring the jupiter lab up, run the below command
+
+`jupyter-lab` in terminal
+
+
+# keyboard shorts in Jupyter lab
+
+if we want to create a new box on top of the row
+ Step 1: First select the starting edge of the row -> press  'a' in keyboard, which adds a new cell and if we want to make the cell as
+markdown to enter the free text and add some heading then just click on keyboard 'm' and type like # Load the dataframe 
+
+IF we want to add new box after the data is printed then, select the focus on last row and click keyboard 'b' and it adds a new box
+
